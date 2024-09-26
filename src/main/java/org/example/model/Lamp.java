a4edce65933ee975174cc8c0f201aa8d4d9fb4bc
Tayfun-senturk <tayfunsenturk8@gmail.com>
2024-09-26 03:43:30 +0300

    Done

---
+++ src/main/java/org/example/model/Lamp.java
@@ -0,0 +1,32 @@
+package org.example.model;
+
+
+import org.example.model.enums.LampType;
+
+public class Lamp {
+    private int globRating;
+    private boolean battery;
+    private LampType style;
+
+    public Lamp(LampType style, boolean battery,int globRating) {
+        this.globRating = globRating;
+        this.battery = battery;
+        this.style = style;
+    }
+
+    public void turnOn(){
+        System.out.println("Lamp is being turned on.");
+    }
+
+    public int getGlobRating() {
+        return globRating;
+    }
+
+    public boolean isBattery() {
+        return battery;
+    }
+
+    public LampType getStyle() {
+        return style;
+    }
+}
